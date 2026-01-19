5572ead3db16b889b364c82dd081113c694d5643
Aras <bbproyes@gmail.com>
2026-01-19 10:56:07 +0300

    Add files via upload

---
+++ destek.js
@@ -0,0 +1,7 @@
+console.log("Bu Site Aras Tarafından Tek Başına Yapılmıştır.");
+console.log("This Page İs Developed By Only Aras.");
+console.log("");
+console.log("Destek Olmak İster Misiniz?");
+console.log("Would You Like To Help My Page?");
+console.log("");
+console.log('"bbproyes@gmail.com"');
