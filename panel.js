4274d295907e9568850ffd349f7df36b0bf38834
Aras <bbproyes@gmail.com>
2026-02-07 16:28:46 +0300

    Update panel.js

--- panel.js
+++ panel.js
@@ -8,6 +8,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyAVanlbwHO2CFO_45R9ez7Os6v4h5y64bM",
   authDomain: "login-aras-siteler.firebaseapp.com",
   projectId: "login-aras-siteler",
+  storageBucket: "login-aras-siteler.firebasestorage.app",
+  messagingSenderId: "236271357844",
+  appId: "1:236271357844:web:6ad66b5e19bc49ea3cc981",
+  measurementId: "G-ZLEH0TKWLK"
 };
 
 const app = initializeApp(firebaseConfig);
