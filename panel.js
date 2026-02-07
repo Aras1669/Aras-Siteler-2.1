f2f706b657f04a5f1194c398383f25e85209b9e1
Aras <bbproyes@gmail.com>
2026-02-07 16:25:27 +0300

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,62 +1,44 @@
-alert("Panel.js çalışıyor");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
-import {
-  getFirestore,
-  doc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-
-
+alert("panel.js başladı");
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVanlbwHO2CFO_45R9ez7Os6v4h5y64bM",
   authDomain: "login-aras-siteler.firebaseapp.com",
   projectId: "login-aras-siteler",
-  storageBucket: "login-aras-siteler.firebasestorage.app",
-  messagingSenderId: "236271357844",
-  appId: "1:236271357844:web:6ad66b5e19bc49ea3cc981",
-  measurementId: "G-ZLEH0TKWLK"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const textDiv = document.getElementById("text");
 
-
-// Giriş kontrolü
 onAuthStateChanged(auth, async (user) => {
 
+  alert("Auth kontrolü geçti");
+
   if (!user) {
+    alert("Login yok");
     window.location.href = "index.html";
     return;
   }
 
-  // Yazı
+  alert("User: " + user.email);
+
+  alert("Firestore okunuyor...");
+
   const ref = doc(db, "BuArasinGizli", "BuArasinGizli");
+
   const snap = await getDoc(ref);
 
+  alert("Snap geldi");
+
   if (snap.exists()) {
-    textDiv.innerText = snap.data().content;
+    document.getElementById("text").innerText = snap.data().content;
   } else {
-    textDiv.innerText = "İçerik bulunamadı.";
+    document.getElementById("text").innerText = "Veri yok";
   }
 
 });
-
-
-// Çıkış
-window.logout = () => {
-  signOut(auth).then(() => {
-    window.location.href = "index.html";
-  });
-};
