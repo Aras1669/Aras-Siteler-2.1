adbef3194df9b618d47bcc6655b85dcdc967eaa7
Aras <bbproyes@gmail.com>
2026-02-07 19:21:46 +0300

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,11 +1,13 @@
-alert("panel.js başladı ✅");
-
-// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyAVanlbwHO2CFO_45R9ez7Os6v4h5y64bM",
   authDomain: "login-aras-siteler.firebaseapp.com",
   projectId: "login-aras-siteler",
+  storageBucket: "login-aras-siteler.firebasestorage.app",
+  messagingSenderId: "236271357844",
+  appId: "1:236271357844:web:6ad66b5e19bc49ea3cc981",
+  measurementId: "G-ZLEH0TKWLK"
 };
+
 
 // Init
 firebase.initializeApp(firebaseConfig);
@@ -33,22 +35,28 @@ auth.onAuthStateChanged(async (user) => {
     alert("Firestore okunuyor...");
 
     const doc = await db
-      .collection("BuArasinGizli")
-      .doc("BuArasinGizli")
+      .collection("main")
+      .doc("main")
       .get();
 
     alert("Firestore cevap verdi ✅");
 
     if (doc.exists) {
+
       document.getElementById("text").innerText =
-        doc.data().content;
+        doc.data().GizliYazi;
+
     } else {
+
       document.getElementById("text").innerText =
         "Veri yok";
+
     }
 
   } catch (e) {
+
     alert("Hata: " + e.message);
+
   }
 
 });
